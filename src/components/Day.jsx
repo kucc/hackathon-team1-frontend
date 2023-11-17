26bd6c8abd980600979ac9e42578851853476cb9
juknow <124476542+juknow@users.noreply.github.com>
2023-11-18 00:56:54 +0900

    [feat] 수정 전 저장

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,11 +1,13 @@
 import DayToDo from './DayToDo';
 
+const Daydate = '2022-10-15';
+
 function Day() {
     return (
         <div className="w-[704px] h-auto relative flex-column  justify-center items-center">
             <div className="w-[632px] h-[124px] pl-[68px] pr-[87px] left-[36px] top-[54px] absolute bg-gray-900 rounded-[10px] justify-start items-center inline-flex">
                 <div className="w-[477px] h-6 text-center text-white text-[32px] font-bold font-['Noto Sans KR'] leading-[48px]">
-                    2023년 1월 30일
+                    {Daydate}
                 </div>
             </div>
             <div className="w-[632px] h-[500px] pl-[15px] pr-[34px] pt-[50px] pb-[124px] left-[36px] top-[204px] absolute bg-gray-900 rounded-[10px] flex-col justify-start items-center gap-[108px] inline-flex">
