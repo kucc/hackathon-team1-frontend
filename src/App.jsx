ee62ac58d06a860b044c7716592e8f5a8a809017
juknow <124476542+juknow@users.noreply.github.com>
2023-11-17 23:58:26 +0900

    [feat] 라우터 추가
    라우터 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import 'tailwindcss/tailwind.css';
 import Calendar from './Calender';
 import User from './Users';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
         <div>
-            <Calendar />
-            <User />
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/about" element={<User />} />
+                </Routes>
+            </Router>
         </div>
     );
 }
